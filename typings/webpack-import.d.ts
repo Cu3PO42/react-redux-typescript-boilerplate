d6a55e5aea32e4025f17896491e2e70c8a248993
Copper Phosphate <cu3po42@gmail.com>
2017-04-08 00:27:22 +0200

    Allow TypeScript to import CSS files

---
+++ typings/webpack-import.d.ts
@@ -0,0 +1,10 @@
+declare module '*.css' {
+  const names: {[name: string]: string};
+  export default names;
+}
+
+
+declare module '*.scss' {
+  const names: {[name: string]: string};
+  export default names;
+}
